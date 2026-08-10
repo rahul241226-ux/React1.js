39d1da523e0f07bb41f5c7e7189b500bdc2b7b4a
Rahul <rahul.241226@ncit.edu.np>
2026-08-10 22:18:39 +0545

    Project09:themechanger

---
+++ 09themeswitcher/src/context/theme.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+
+export const ThemeContext = createContext({
+
+themeMode: "light",
+
+darkTheme:()=>{},
+lightTheme:()=>{},
+
+
+ })
+
+ export const ThemeProvider = ThemeContext.Provider
+
+
+ //hooks
+
+ export default function useTheme(){
+
+return useContext(ThemeContext);
+
+
+ }
